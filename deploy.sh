5dff7584a513597653e6a79b542a31dc485b4fc3
dodo <empty>
2022-08-30 13:42:26 +0200

    fixed typos in 2 files

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 if [ ! -d "app/.virtualenv" ]; then
     echo "You have not set up a virtual enviroment for testing the application"
     echo "Step 1: CD into app folder and run 'python3 -m venv .virtualenv'"
-    echo "Step 2: Activate virtualenv and run 'source .virtualenv/bin/activate'"
+    echo "Step 2: Run 'source .virtualenv/bin/activate' to activate virtual enviroment"
     echo "Step 3: Run 'pip install -r requirements.txt'"
     exit
 fi
