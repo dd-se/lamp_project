1dc0f89df422a6c62eb85b8061740af16a3ae9ca
dodo <empty>
2022-09-04 16:20:33 +0200

    added tf automation variable for decreased output during deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export TF_IN_AUTOMATION=YES
 if [ ! -d "app/.virtualenv" ]; then
     echo "You have not set up a virtual enviroment for testing the application"
     echo "Step 1: CD into app folder and run 'python3 -m venv .virtualenv'"
@@ -15,8 +16,8 @@ echo "Tests passed, deploying..."
 cd ../provisioner || exit
 terraform apply --auto-approve || exit
 echo "Provisioning done, creating ansible inventory."
-LB=$(terraform output loadbalancer | jq -r)
-DB=$(terraform output database | jq -r)
+LB=$(terraform output -raw loadbalancer)
+DB=$(terraform output -raw database)
 BACKENDS=$(terraform output -json backends | jq -r .[])
 cd .. || exit
 cat <<EOF >hosts
